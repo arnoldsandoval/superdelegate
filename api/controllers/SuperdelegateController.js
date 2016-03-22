4f7c7754415c68acc164ceb9345f506e315873ab
Arnold Sandoval <arnoldsandoval@google.com>
2016-03-21 18:56:40 -0700

    add sails

---
+++ api/controllers/SuperdelegateController.js
@@ -0,0 +1,11 @@
+/**
+ * SuperdelegateController
+ *
+ * @description :: Server-side logic for managing superdelegates
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
